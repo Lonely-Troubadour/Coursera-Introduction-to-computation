38310da40e69fac782a93ae16bd26a9486fb2091
Mr.Who <32093451+Lonely-Troubadour@users.noreply.github.com>
2018-02-18 18:29:57 +0800

    Create ＃1： 晶晶赴约会.cpp

---
+++ Week 5/＃1： 晶晶赴约会.cpp
@@ -0,0 +1,22 @@
+//#1 晶晶赴约会
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int day=0;
+
+	cin>>day;
+
+	switch (day){
+		case 1 : cout<<"NO"<<endl;
+			break;
+		case 3 : cout<<"NO"<<endl;
+			break;
+		case 5 : cout<<"NO"<<endl;
+			break;
+		default : cout<<"YES"<<endl;
+	}
+	return 0;
+}
